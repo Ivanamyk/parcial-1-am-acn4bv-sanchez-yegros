b315325aac6da4a5e26f7d8a3de7e237374c0b50
MatiasSanchez <mati_sanchez23@hotmail.com>
2023-10-21 23:51:59 -0300

    feat: se agrego Onclick al button para conectar la otra activity

--- app/src/main/java/com/example/parcial_1_am_acn4bv_sanchez_yegros/MainActivity.java
+++ app/src/main/java/com/example/parcial_1_am_acn4bv_sanchez_yegros/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.parcial_1_am_acn4bv_sanchez_yegros;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
-import android.graphics.LinearGradient;
-import android.graphics.Shader;
-import android.graphics.drawable.ShapeDrawable;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.Gravity;
-import android.widget.Button;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +21,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         LinearLayout layout = findViewById(R.id.content); // Reemplaza 'content' con el ID de tu LinearLayout
+
+        TextView textoIntro = new TextView(this);
+
+        textoIntro.setId(View.generateViewId());
+
+        textoIntro.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
+        textoIntro.setText(getResources().getString(R.string.Text_Start));
+        textoIntro.setGravity(Gravity.CENTER);
+        textoIntro.setPadding(0,0,0,50);
+        textoIntro.setTextColor(getResources().getColor(R.color.white));
+        textoIntro.setTextSize(TypedValue.COMPLEX_UNIT_SP,18);
+        //ConstraintLayout constraintLayout = findViewById(R.id.constraint);
+        //constraintLayout.addView(textoIntro);
+        layout.addView(textoIntro);
+
+    }
+
+    public void openHome(View view){
+        Intent intent = new Intent(this,Main_Page.class);
+        startActivity(intent);
 
     }
 }
