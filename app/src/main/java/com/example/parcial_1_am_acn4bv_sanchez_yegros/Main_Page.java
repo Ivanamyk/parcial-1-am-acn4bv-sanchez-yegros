5d6479e45a9a826d5edd1a549bef41e8c0b164d2
MatiasSanchez <mati_sanchez23@hotmail.com>
2023-10-23 21:20:08 -0300

    feat: refactor id imagenes y se agrego un toast

--- app/src/main/java/com/example/parcial_1_am_acn4bv_sanchez_yegros/Main_Page.java
+++ app/src/main/java/com/example/parcial_1_am_acn4bv_sanchez_yegros/Main_Page.java
@@ -3,6 +3,9 @@ package com.example.parcial_1_am_acn4bv_sanchez_yegros;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class Main_Page extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class Main_Page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+        ImageView imagePearl = findViewById(R.id.pearl);
+
+        imagePearl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"proximante una nueva vista",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
