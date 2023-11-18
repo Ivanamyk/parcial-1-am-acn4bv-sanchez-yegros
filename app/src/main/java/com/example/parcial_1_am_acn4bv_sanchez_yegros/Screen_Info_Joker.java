60b7af6591fc1c9a9e9330e22c21d6d9314c47cd
ivana <ivanamyk@example.com>
2023-11-18 20:55:33 -0300

    agregue las paginas de info y trailers de joker + pearl

---
+++ app/src/main/java/com/example/parcial_1_am_acn4bv_sanchez_yegros/Screen_Info_Joker.java
@@ -0,0 +1,36 @@
+package com.example.parcial_1_am_acn4bv_sanchez_yegros;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class Screen_Info_Joker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_joker);
+
+        //joker trailer
+        VideoView videoView = findViewById(R.id.videoViewJoker);
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.joker;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
+
+    }
+
+
+    public void backMainPage(View view){
+        Intent intent = new Intent(this,Main_Page.class);
+        startActivity(intent);
+    }
+}
