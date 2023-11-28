a4da54cb7c3cff3b57f0e4aa5906c6d087c52111
ivana <ivanamyk@example.com>
2023-11-28 12:52:03 -0300

    agrego check de la conectividad a internet

--- app/src/main/java/com/example/parcial_1_am_acn4bv_sanchez_yegros/MainActivity.java
+++ app/src/main/java/com/example/parcial_1_am_acn4bv_sanchez_yegros/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.parcial_1_am_acn4bv_sanchez_yegros;
 
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.TypedValue;
 import android.view.Gravity;
@@ -20,10 +22,26 @@ public class MainActivity extends AppCompatActivity {
     ImageView imageView;
     Animation scaleUp, scaleDown;
 
+    public void checkConnection(){
+        LinearLayout noInternetMsg = findViewById(R.id.noInternetMsg);
+        LinearLayout mainPage = findViewById(R.id.content);
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        if(networkInfo != null && networkInfo.isConnected()){
+            noInternetMsg.setVisibility(View.INVISIBLE);
+            mainPage.setVisibility(View.VISIBLE);
+        }else{
+            noInternetMsg.setVisibility(View.VISIBLE);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkConnection();
 
         LinearLayout layout = findViewById(R.id.content); // Reemplaza 'content' con el ID de tu LinearLayout
 
