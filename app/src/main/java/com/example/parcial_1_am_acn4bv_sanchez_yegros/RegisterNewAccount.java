ba5e34503defa03342f6b0d97cf32660d843bada
MatiasSanchez <mati_sanchez23@hotmail.com>
2023-12-17 00:05:57 -0300

    feat: ase agrego metodo addToFireStore

--- app/src/main/java/com/example/parcial_1_am_acn4bv_sanchez_yegros/RegisterNewAccount.java
+++ app/src/main/java/com/example/parcial_1_am_acn4bv_sanchez_yegros/RegisterNewAccount.java
@@ -2,6 +2,7 @@ package com.example.parcial_1_am_acn4bv_sanchez_yegros;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -10,16 +11,42 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import java.util.Map;
+import java.util.HashMap;
 
 public class RegisterNewAccount extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseFirestore db;
 
+    private void addToFirestore(String email, String password) {
+        Map<String, Object> user = new HashMap<>();
+        user.put("email", email);
+        user.put("password", password);
+
+        // Add a new document with a generated ID
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d("TAG", "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("TAG", "Error adding document", e);
+                    }
+                });
+    }
 
     public void register(String email, String password){
         mAuth.createUserWithEmailAndPassword(email, password)
@@ -28,6 +55,7 @@ public class RegisterNewAccount extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             Toast.makeText(RegisterNewAccount.this,"Nuevo Usuario",Toast.LENGTH_LONG).show();
+                            addToFirestore(email, password);
                             Intent intent = new Intent(getApplicationContext(),MainActivity.class);
                             startActivity(intent);
                         } else {
