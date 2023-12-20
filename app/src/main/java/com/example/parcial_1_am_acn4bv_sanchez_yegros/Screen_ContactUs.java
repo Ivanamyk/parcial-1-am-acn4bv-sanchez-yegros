44120343dfc9f04d91072450a867a69c8e689b86
MatiasSanchez <mati_sanchez23@hotmail.com>
2023-12-19 23:04:25 -0300

    feat:  add usuario en views: Contacto,Mid,Btf y Moon

--- app/src/main/java/com/example/parcial_1_am_acn4bv_sanchez_yegros/Screen_ContactUs.java
+++ app/src/main/java/com/example/parcial_1_am_acn4bv_sanchez_yegros/Screen_ContactUs.java
@@ -1,5 +1,6 @@
 package com.example.parcial_1_am_acn4bv_sanchez_yegros;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,17 +9,28 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class Screen_ContactUs extends AppCompatActivity {
     EditText et_subject, et_message;
     Button btn;
     private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
+
+    private Usuario user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen_contact_us);
 
@@ -57,6 +69,36 @@ public class Screen_ContactUs extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            String uid = currentUser.getUid();
+            db
+                    .collection("users")
+                    .whereEqualTo("uid",uid)
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if(task.isSuccessful()){
+                                for(QueryDocumentSnapshot documento: task.getResult()){
+                                    String id = documento.getId();
+                                    Object data = documento.getData();
+                                    user = documento.toObject(Usuario.class);
+                                    TextView textViewUser = findViewById(R.id.textViewUserContacto);
+                                    if (textViewUser != null) {
+                                        textViewUser.setText(user.getUserName());
+                                    }
+
+                                }
+                            }
+                        }
+                    });
+        }}
     public void backMainPage(View view){
         Intent intent = new Intent(this,Main_Page.class);
         startActivity(intent);
--- app/src/main/java/com/example/parcial_1_am_acn4bv_sanchez_yegros/Screen_Info_Moonlight.java
+++ app/src/main/java/com/example/parcial_1_am_acn4bv_sanchez_yegros/Screen_Info_Moonlight.java
@@ -1,5 +1,6 @@
 package com.example.parcial_1_am_acn4bv_sanchez_yegros;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,16 +8,27 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.MediaController;
+import android.widget.TextView;
 import android.widget.VideoView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class Screen_Info_Moonlight extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
+
+    private Usuario user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_moonlight);
 
@@ -30,6 +42,35 @@ public class Screen_Info_Moonlight extends AppCompatActivity {
         mediaController.setAnchorView(videoView);
 
     }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            String uid = currentUser.getUid();
+            db
+                    .collection("users")
+                    .whereEqualTo("uid",uid)
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if(task.isSuccessful()){
+                                for(QueryDocumentSnapshot documento: task.getResult()){
+                                    String id = documento.getId();
+                                    Object data = documento.getData();
+                                    user = documento.toObject(Usuario.class);
+                                    TextView textViewUser = findViewById(R.id.textViewUserMoon);
+                                    if (textViewUser != null) {
+                                        textViewUser.setText(user.getUserName());
+                                    }
+
+                                }
+                            }
+                        }
+                    });
+        }}
 
 
     public void backMainPage(View view){
